fd548396907c224ea96dace2ec61539377508ce0
Steven Yu <stevenyuser@gmail.com>
2024-01-16 17:25:49 -0500

    added error handling

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,18 @@ app.get('/', (_req: Request, res: Response) => {
 })
 
 app.get('/scrape', async (_req: Request, res: Response) => {
-    const c2cData = await puppeteerScraper();
+    try {
+        const c2cData = await puppeteerScraper();
 
-    res.status(200).send({
-        message: "success",
-        data: c2cData,
-    });
+        res.status(200).send({
+            message: "success",
+            data: c2cData,
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: `ERROR: an error occurred in the /scrapes endpoint: ${err}`,
+        });
+    }
 })
 
 app.get('/ping', (_req: Request, res: Response) => {
