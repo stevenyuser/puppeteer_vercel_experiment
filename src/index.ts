c24acb5356b15fa1e9257a4d1f52f1c4101a19ed
Steven Yu <stevenyuser@gmail.com>
2024-01-16 17:21:51 -0500

    added scraper

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from 'express'
+import { puppeteerScraper } from './scraper'
 
 const app = express()
 const port = process.env.PORT || 8080
 
 app.get('/', (_req: Request, res: Response) => {
     return res.send('Hello world!')
+})
+
+app.get('/scrape', async (_req: Request, res: Response) => {
+    const c2cData = await puppeteerScraper();
+
+    res.status(200).send({
+        message: "success",
+        data: c2cData,
+    });
 })
 
 app.get('/ping', (_req: Request, res: Response) => {
---
+++ src/scraper.ts
@@ -0,0 +1,79 @@
+import puppeteer from 'puppeteer-core'
+import chromium from '@sparticuz/chromium-min'
+
+// code from: https://github.com/stefanjudis/tiny-helpers/blob/primary/api/screenshot.js
+async function getBrowser() {
+    // local development is broken for this 👇
+    // but it works in vercel so I'm not gonna touch it
+    return puppeteer.launch({
+      args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(
+        `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+      ),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+}
+
+export const puppeteerScraper = async () => {
+    const browser = await getBrowser();
+    
+    const page = await browser.newPage();
+
+    await page.goto('https://c2cbus.ipp.cornell.edu/mobile/?a=mobile');
+
+    // Selects travelers
+    await page.select('#ctl00_cph_ddlQty', '1');
+
+    // Selects one way
+    await page.select('#ctl00_cph_ddlTripType', 'One Way');
+
+    // Selects pickup location as Ithaca, North Campus
+    await page.select('#ctl00_cph_ddlDepPickLocation', '7,16');
+
+    // // Selects dropoff location as New York City, Cornell Club
+    await page.waitForSelector('#ctl00_cph_ddlDepDropLocation');
+    await page.select('#ctl00_cph_ddlDepDropLocation', '8,19');
+
+    // Closes popup message if there is one
+    try {
+        await page.waitForSelector('#msgClose.btn', {timeout: 2000, visible: true});
+
+        await page.click('#msgClose.btn');
+    } catch (e) {
+        console.log("No popup");
+    }
+
+    // Clicks date picker
+    await page.click('#ctl00_cph_txtDepDate');
+
+    // Selects date (tomorrow for testing)
+    // Increases day by one
+    await page.waitForXPath('/html/body/form/div[5]/div[3]/div[4]/div[2]/div[2]/table/tr[4]/td[2]');
+    await page.click('xpath//html/body/form/div[5]/div[3]/div[4]/div[2]/div[2]/table/tr[4]/td[2]');
+
+    // // Selects set button
+    await page.click("xpath//html/body/form/div[5]/div[3]/div[4]/div[2]/div[2]/table/tr[5]/td[3]");
+
+    // Prints out the possible tickets for the date
+    const date = new Date();
+    date.setDate(date.getDate() + 1);
+    console.log(date.toDateString() + ":");
+
+    await page.waitForSelector('label.radio');
+    const elements = await page.$$('label.radio');
+
+    const tripData: string[] = [];
+
+    for (const element of elements) {
+        const text = await element.$eval('span', el => el.textContent);
+        tripData.push(text);
+    }
+
+    console.log(tripData);
+
+    await browser.close();
+
+    return tripData;
+} 
